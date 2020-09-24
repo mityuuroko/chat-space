8bac17501cfd00f25b3b6bc0b698d9ba35e826cb
mityuuroko <genius320320@gmail.com>
2020-09-24 23:52:16 +0900

    u

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -41,7 +41,7 @@ $(function(){
   }
 
   let reloadMessages = function() {
-    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
+    let last_message_id = $('.Message-box:last').data("message-id") || 0;
     $.ajax({
 
       url: "api/messages",
